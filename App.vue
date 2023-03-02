2847f5aed3e5eb098904cf241fc829d7f5212f96
ㅇ ㅠ ㄴ ㅅ ㅐ ㅁ <48478079+PhoebeYoon@users.noreply.github.com>
2023-03-02 21:16:47 +0900

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,53 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>created by Vue3</p>
+    <div v-if="showModal"  >
+      <Modal  theme="sale"  @close="toggleWin">
+        <!-- <h2>UVC</h2>
+        <p>Learing font-end-developer course</p> -->
+        <h2>UVC</h2>
+        <p>Learing font-end-developer course</p>
+
+        <template v-slot:links>   
+          <a href="#">아이디 찾기</a> 
+          <a href="#">비번 찾기</a>
+        </template>
+
+      </Modal>
+    </div>
+    <button @click="toggleWin">팝업창보기</button>
+</template>
+<script>
+import Modal from './components/Modal.vue'
+export default {
+  name :'App',
+  components :{ Modal}, 
+  data(){
+    return {
+      title :"My First Vue Application",
+      comments:"회원가입 | 로그인",
+      text:'가입혜택이 있습니다',
+      showModal:false
+    }
+  },
+  methods:{
+    toggleWin(){
+      this.showModal = !this.showModal
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body { margin: 0;}
+h1 { border-bottom: 2px dashed #000; 
+  padding-bottom:10px; display:inline}
+</style>
---
+++ Modal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="backdrop" @click.self="closeModal"> 
+    <div class="modal" :class="{sale: theme =='sale'}">
+      <slot>이내용은 부모에서 디폴트slot에 들어갈 내용이 사라질때만 보입니다</slot>
+      <!--부모에서 디폴트 <slot>에 들어갈 내용이 사라질때만 보입니다  -->
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['theme'],
+  methods:{ closeModal(){ 
+      //alert('ok')
+      this.$emit('close')
+  }}
+}
+</script>
+<style>
+.modal {
+  width:400px; padding:20px; margin:30% auto;
+  background: white;
+  border-radius: 10px;
+}
+.modal p { color:#fff; font-size: 120%; }
+.backdrop{ top:0; position: fixed;
+  background: rgba(0,0,0,0.5);
+  width: 100%;  height: 100%;}
+.modal.sale { background:crimson; color:#fff }
+.modal.sale h1 { color: #fff;}
+.modal .actions{
+  text-align: center; margin:30px 0 10px 10px;
+}
+.modal .actions a{ color:#333; padding: 8px; 
+  border:1px solid #eee; border-radius: 4px; 
+  text-decoration: none; margin: 10px;}
+.modal.sale .actions a{
+  color:white
+}
+</style>
