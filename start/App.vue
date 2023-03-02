d228b8f049442059e179e77fc67d710ca58ca1b6
ㅇ ㅠ ㄴ ㅅ ㅐ ㅁ <48478079+PhoebeYoon@users.noreply.github.com>
2023-03-02 14:55:54 +0900

    Add files via upload

---
+++ start/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>created by Vue3</p>
+      <Modal :comments="comments" :text ="text" theme="sale" />
+    <button @click="toggleWin">팝업창보기</button>
+</template>
+<script>
+import Modal from './components/Modal.vue'
+export default {
+  name :'App',
+  components :{ Modal}, 
+  data(){
+    return {
+      title :"My First Vue Application",
+      comments:"회원가입 | 로그인",
+      text:'가입혜택이 있습니다'
+    
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body { margin: 0;}
+h1 { border-bottom: 2px dashed #000; 
+  padding-bottom:10px; display:inline}
+</style>
---
+++ start/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="backdrop"> 
+    <div class="modal" :class="{sale: theme =='sale'}">
+      <h2>{{ comments}}</h2>
+      <p> Modal content</p>
+      <span> {{ text }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:[ 'comments','text', 'theme']
+}
+</script>
+<style scoped>
+.modal {
+  width:400px; padding:20px; margin:30% auto;
+  background: white;
+  border-radius: 10px;
+}
+.modal p { color:hotpink; font-size: 120%; }
+.backdrop{ top:0; position: fixed;
+  background: rgba(0,0,0,0.5);
+  width: 100%;  height: 100%;}
+.modal.sale { background:crimson; color:#fff }
+.modal.sale h1 { color: #fff;}
+/*  */
+</style>
