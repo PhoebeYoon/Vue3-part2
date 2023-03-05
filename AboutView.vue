5eb7d60336344627c5c0ada883ad47e776026f97
ㅇ ㅠ ㄴ ㅅ ㅐ ㅁ <48478079+PhoebeYoon@users.noreply.github.com>
2023-03-05 20:20:06 +0900

    Add files via upload

---
+++ AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Rounter를 활용한 예</p>
+  </div>
+</template>
---
+++ HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+   <h1>This is Home page</h1>
+   <p> Router를 활용한 예</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
